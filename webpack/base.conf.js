2efe32d487e4486e6797b5b612ec45ee73537b18
yuanhuihui <yuanhuihui@meituan.com>
2017-10-02 19:48:07 +0800

    favicon

--- webpack/base.conf.js
+++ webpack/base.conf.js
@@ -83,6 +83,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
         new webpack.ProgressPlugin(),
         new CopyWebpackPlugin([{
             from: resolve('statics'),
